90ab746842d4c003d94ddb50d927141997990b99
alumno <alumno@pc-alumno.(none)>
2020-05-21 15:31:31 +0200

    Haciendo el segundo

--- Ejercicio1.sh
+++ Ejercicio1.sh
@@ -2,4 +2,11 @@ read -p "primer numero: " n1
 read -p "segundo numero: " n2
 
 
-if [ $((n1
+if [ $((n1%n2)) -eq 0 ]; 
+then
+
+    echo "Es multiplo";
+else
+
+    echo "no es multiplo";
+fi
---
+++ Ejercicio2.sh~
@@ -0,0 +1,6 @@
+read -p "introduce un numero: " n1
+
+until [  $n1 -eq 1 ]; do
+   echo $n1
+     let (($n1/2))
+done
